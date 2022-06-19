4c465b7946fc4eab06ca210b56be735671a76efa
RafaelJuliano <rafael.j.ferreira@hotmail.com>
2022-06-19 19:36:05 -0300

    lumen pasport configurated

--- ApiGateway/bootstrap/app.php
+++ ApiGateway/bootstrap/app.php
@@ -31,6 +31,7 @@ $app->withEloquent();
  * Registering config files
  */
 $app->configure('services');
+$app->configure('auth');
 
 /*
 |--------------------------------------------------------------------------
@@ -97,8 +98,10 @@ $app->configure('app');
 */
 
 // $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
+$app->register(App\Providers\AuthServiceProvider::class);
+$app->register(Laravel\Passport\PassportServiceProvider::class);
+$app->register(Dusterio\LumenPassport\PassportServiceProvider::class);
 
 /*
 |--------------------------------------------------------------------------
